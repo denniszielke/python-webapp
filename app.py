53eb518565aca1973a1b2217a0165830d54a328b
denniszielke <11569044+denniszielke@users.noreply.github.com>
2023-08-24 14:43:50 +0200

    added details

--- app.py
+++ app.py
@@ -1,32 +1,20 @@
-import os
+from fastapi import FastAPI, Header, Request, status
+from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
+import uvicorn
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+app = FastAPI()
 
-app = Flask(__name__)
+@app.get('/')
+async def default(request: Request):
+    return { 'remoteip': request.client.host, 'realip': request.headers.get('x-real-ip', request.client.host), 'remoteaddr': request.client.host}
 
+@app.get('/ip')
+async def ip(request: Request):
+    return { 'remoteip': request.client.host, 'realip': request.headers.get('x-real-ip', request.client.host), 'remoteaddr': request.client.host}
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
+@app.get('/headers')
+async def header(request: Request):
+    return request.headers
 
 if __name__ == '__main__':
-   app.run()
+    uvicorn.run('main:app', host='0.0.0.0', port=8080)
